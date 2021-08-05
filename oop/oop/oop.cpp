8cf8ec226edda08ccd609962fa5d4e7e384c727b
janghona <jang1na2@gmail.com>
2021-08-06 03:55:23 +0900

    객체2 시작 : 2차원 공간에서 화면 제어
    * Utils.h에 선언된 Borland 함수를 이용해서 콘솔에 2차원 화면 테스트
    * Screen 클래스를 2차원에 출력

--- oop/oop/oop.cpp
+++ oop/oop/oop.cpp
@@ -2,325 +2,79 @@
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 
+#include <iostream>
 #include <conio.h> // console io
 #include <cstring> // string.h
 #include <cstdlib> // stdlib.h
 #include <string> // c++ string class
 #include <Windows.h>
+#include "Utils.h"
 
-const int directionToLeft = 0;
-const int directionToRight = 1;
-
-class Screen;
-class Player;
-class Enemy;
-class Bullet;
+using namespace std;
 
 class Screen {
 private:
-	int size;
-	char* canvas;
+	int width;
+	int height;
+	char* buffer;
+
 public:
-	Screen(unsigned int screenSize) {
-		if (size == 0) size = 80;
-		size = screenSize;
-		canvas = new char[size + 1];
+	Screen(int width= 10, int height = 10)
+	:width(width), height(height), buffer(new char[getSize()])
+	{
+		Borland::initialize();
+		buffer[getSize() - 1] = '\0';
 	}
 
 	~Screen() {
-		delete[] canvas;
-		canvas = nullptr;
-		size = 0;
+		delete[] buffer;
 	}
 
+	int getWidth() const { return width; }
+	int getScreenWidth() const { return width + 1; }
+	int getHeight() const { return height; }
+	int getSize() const { return getScreenWidth() * height; }
+
 	void clear() {
-		memset(canvas, ' ', size); // clear screen
+		memset(buffer, '*', getSize());
+		buffer[getSize() - 1] = '\0';
 	}
 
 	void render() {
-		canvas[size] = '\0';  // render screen
-		printf("%s\r", canvas);
-	}
-
-	void draw(int pos, char face) {
-		if (pos < 0 || pos >= size) return;
-		canvas[pos] = face;
-	}
-
-	void draw(int pos, const char* face) {
-		strncpy(&(canvas[pos]), face, strlen(face)); // for bullet draw
-	}
-
-	bool isInRange(Bullet* bullet);
-};
-class GameObject {
-private:
-	char face[20];
-	int pos;
-	int direction;
-	Screen* screen;
-	GameObject** gameObjects;
-
-public:
-	GameObject(GameObject** gameObjects,Screen* screen,const char* face, int pos,int direction)
-		: pos(pos), direction(direction), screen(screen), gameObjects(gameObjects)
-	{
-		setFace(face);
-	}
-
-	virtual ~GameObject() {}
-
-	int getPos() { return pos; }
-	void setPos(int pos) { this->pos = pos; }
-
-	const char* getFace() { return face; }
-	void setFace(const char* face) { strcpy(this->face, face); }
-
-	int getDirection() { return direction; }
-	void setDirection(int direction) { this->direction = direction; }
-
-	Screen* getScreen() { return screen; }
-	GameObject** getGameObjects() { return gameObjects; }
-
-	virtual void draw() {
-		screen->draw(pos, face);
-	}
-
-	virtual void update() {}
-
-	void move(int direction){
-		direction == directionToRight ? pos++ : pos--;
-	}
-	void move(){
-		direction == directionToLeft ? --pos : ++pos;
-	}
-};
-
-class Player : public GameObject{
-private:
-	int nRemaining;
-	char		originalFace[20];
-
-public:
-	Player(GameObject** gameObjects,Screen* screen, const char* face, int pos);
-	~Player() override {	}
-
-	void draw() override;
-	void update() override;
-
-	void fire();
-	Bullet* find_unused_bullet();
-	Enemy* find_enemy();
-
-	void onEnemyHit() {
-		setFace("(^_^)");
-		nRemaining = 20;
-	}
-};
-
-class Enemy : public GameObject {
-private:
-	int nRemaining;
-	char	originalFace[20];
-
-public:
-	Enemy(GameObject** gameObjects,Screen* screen,const char* face, int pos)
-		: GameObject(gameObjects,screen,face,pos,directionToLeft), nRemaining(0)
-	{
-		strcpy(originalFace, face);
-	}
-
-	void onHit() {
-		setFace("(T_T)");
-		nRemaining = 10;
-	}
-
-	void update() override
-	{
-		if (nRemaining == 0) return;
-		--nRemaining;
-		if (nRemaining == 0) setFace(originalFace);
-	}
-
-	bool isHit(Bullet* bullet);
-};
-
-class Bullet : public GameObject{
-private:
-	bool isReady;
-	
-public:
-	Bullet(GameObject** gameObjects,Screen* screen)
-	:GameObject(gameObjects,screen,"-",0,directionToLeft), isReady(true)
-	{
-	}
-
-	~Bullet() override {}
-
-	void makeReady(){
-		isReady = true;
-	}
-
-	void update() override{
-		if (isReady == true) return;
-
-		move();
-
-		GameObject** gameObjects = getGameObjects();
-		Player* player = nullptr;
-		for (int i = 0;i < 82;i++) {
-			GameObject* obj = gameObjects[i];
-			player = dynamic_cast<Player*>(obj);
-			if (player != nullptr) break;
-			
+		for (int y = 0; y < height - 1; y++) {
+			buffer[y * getScreenWidth() + width] = '\n';
 		}
+		buffer[getSize() - 1] = '\0';
 
-		for (int i = 0; i < 82;i++) {
-			GameObject* obj = gameObjects[i];
-			Enemy* enemy = dynamic_cast<Enemy*>(obj);
-			if (enemy == nullptr) continue;
-			if (enemy->isHit(this)){ // 적이 총알을 맞았을 때
-					enemy->onHit();
-					player->onEnemyHit();
-					makeReady();
-					break;
-			}
-		}
-		Screen* screen = getScreen();
-		if (!screen->isInRange(this)) makeReady();
+		Borland::gotoxy(0, 0);
+		cout << buffer;
 	}
 
-	void setFire(Player* player, Enemy* enemy) {
-		isReady = false; // 사용중
-		int enemy_pos = enemy->getPos();
-		int player_pos = player->getPos();
-		const char* player_face = player->getFace();
-
-		setDirection(directionToLeft);
-		if (player_pos < enemy_pos) setDirection(directionToRight);
-
-		setPos(player_pos);
-		if (getDirection() == directionToRight)
-			setPos(getPos() + (strlen(player_face) - 1));
+	void draw(int x, int y,char shape) {
+		buffer[y * getScreenWidth() + x] = shape;
 	}
-
-	void draw() override {
-		if (isReady == true) return;
-		GameObject::draw();
-	}
-
-	bool isAvailable() { return isReady; }
 };
-
-// 전방위 선언 중 사용할 함수
-bool Screen::isInRange(Bullet* bullet){
-	int bullet_pos = bullet->getPos();
-	return bullet_pos >= 0 && bullet_pos < size;
-}
-
-Player::Player(GameObject** gameObjects,Screen* screen, const char* face, int pos)
-	: GameObject(gameObjects,screen, face, pos, directionToRight), nRemaining(0)
-{
-	strcpy(originalFace, face);
-}
-
-void Player::fire(){
-	Bullet* bullet = find_unused_bullet();
-	if (bullet == nullptr) return;
-	Enemy* enemy = find_enemy();
-	if (enemy == nullptr) return;
-	bullet->setFire(this, enemy);
-}
-
-Bullet* Player::find_unused_bullet(){
-	GameObject** gameObjects = getGameObjects();
-	for (int i = 0; i < 82; i++){
-		GameObject* obj = gameObjects[i];
-		Bullet* bullet = dynamic_cast<Bullet*>(obj);
-		if (bullet == nullptr) continue;
-		if (bullet->isAvailable() == true) return bullet;
-	}
-	return nullptr;
-}
-
-Enemy* Player::find_enemy() {
-	GameObject** gameObjects = getGameObjects();
-	for (int i = 0; i < 82;i++) {
-		GameObject* obj = gameObjects[i];
-		Enemy* enemy = dynamic_cast<Enemy*>(obj);
-		if (enemy == nullptr) continue;
-		return enemy;
-	}
-}
-
-void Player::draw() {
-	GameObject::draw();
-}
-
-void Player::update() {
-	if (nRemaining == 0) return;
-	--nRemaining;
-	if (nRemaining == 0) setFace(originalFace);
-}
-
-bool Enemy::isHit(Bullet* bullet)
-{
-	int bullet_direction = bullet->getDirection();
-	int bullet_pos = bullet->getPos();
-	return bullet_pos >= getPos() && bullet_pos < getPos() + strlen(getFace()) - 1;
-}
 
 int main()
 {
-	Screen screen(80);
-	GameObject* gameObjects[80 + 1 + 1];
-
-	gameObjects[0] = new Player(gameObjects, &screen, "(-_-)", 50);
-	gameObjects[1] = new Enemy(gameObjects, &screen, "(`_#)", 10);
-
-	for (int i = 0; i < 80; i++)
-		gameObjects[i + 2] = new Bullet(gameObjects, &screen);
-
-	int major;
-	int minor;
-
-	// game loop
-	while (true) {
-		screen.clear();
-
-		for (int i = 0; i < 82; i++) gameObjects[i]->update();
-		for (int i = 0; i < 82; i++) gameObjects[i]->draw();
+	Screen screen{ 10, 10 };
 	
+	bool requestExit = false;
+	int x = 0;
+	int y = 0;
+
+	while (requestExit == false) {
+
+		screen.clear();
+		screen.draw(x, y, '0' + x);
 		screen.render();
+
+		//debugging
+		Borland::gotoxy(20, 20);
+		printf("x = %d, y = %d", x, y);
+
 		Sleep(100);
-
-		// handle inputs
-		if (!_kbhit()) continue;
-			major = _getch();
-			switch (major) {
-			case ' ':
-				static_cast<Player*>(gameObjects[0])->fire();
-				break;
-			case 224: // arrow key, function key pressed
-				minor = _getch();
-				switch (minor) {
-				case 75: // left
-					gameObjects[0]->move(directionToLeft);
-					break;
-				case 77: // right
-					gameObjects[0]->move(directionToRight);
-					break;
-				case 72: // up
-					break;
-				case 80: // down
-					break;
-				}
-				break;
-		    }
 	}
-
-	for (int i = 0; i < 82; i++) delete gameObjects[i];
-
 	printf("\nGame Over\n");
 	return 0;
 }
