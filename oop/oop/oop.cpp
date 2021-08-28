7956e9b7b2272909b224d9bca729cfb633416147
janghona <jang1na2@gmail.com>
2021-08-29 04:30:42 +0900

    Singleton pattern(싱글톤 패턴) 적용
    * Screen클래스 생성된 인스턴스 1개만 가지도록 만듬(생성자함수 한번만 하는것)
    * 싱글톤 만드는 방법
    1. 생성자함수를 private 으로 변경
    2. private영역에 static instance 변수 선언(전역변수화)
    3. public영역에서 static getInstance()함수로 생성자 한번 호출(전역함수화)
    * 사용하는 이유 : 1. 사용하지 않을때 인스턴스화 하지 않음 / 2. 런타임에 초기화됨 / 3.싱글톤 상속이 가능
    * 단점 : 1.전역변수로 사용 시 가독성이 떨어짐 / 2. 멀티스레드 프로그래밍에서 전역변수 남용은 교착상태를 일으킬 확률이 큼

--- oop/oop/oop.cpp
+++ oop/oop/oop.cpp
@@ -8,8 +8,7 @@
 #include <cstdlib> // stdlib.h
 #include <string> // c++ string class
 #include <Windows.h>
-#include "Utils.h"
-
+#include"Utils.h"
 using namespace std;
 
 class Screen {
@@ -18,17 +17,22 @@ private:
 	int height;
 	char* buffer;
 
-public:
-	Screen(int width= 10, int height = 10)
-	:width(width), height(height), buffer(new char[getSize()])
-	{
+	static Screen* instance;
+
+	Screen(int width = 10, int height = 10)
+		: width(width), height(height), buffer(new char[getSize()]) {
 		Borland::initialize();
 		buffer[getSize() - 1] = '\0';
 	}
+public:
 
-	~Screen() {
-		delete[] buffer;
+	static Screen* getInstance() {
+		if (instance == nullptr) {
+			instance = new Screen(10,10);
+		}
+		return instance;
 	}
+	~Screen() { delete[] buffer; }
 
 	int getWidth() const { return width; }
 	int getScreenWidth() const { return width + 1; }
@@ -45,29 +49,29 @@ public:
 			buffer[y * getScreenWidth() + width] = '\n';
 		}
 		buffer[getSize() - 1] = '\0';
-
 		Borland::gotoxy(0, 0);
 		cout << buffer;
 	}
 
-	void draw(int x, int y,char shape) {
+	void draw(int x, int y, char shape) {
 		buffer[y * getScreenWidth() + x] = shape;
 	}
 };
 
+Screen* Screen::instance = nullptr;
+
 int main()
 {
-	Screen screen{ 10, 10 };
-	
+	Screen* screen = Screen::getInstance();
+
 	bool requestExit = false;
-	int x = 0;
-	int y = 0;
-
+	int x = 0, y = 0;
+	
 	while (requestExit == false) {
-
-		screen.clear();
-		screen.draw(x, y, '0' + x);
-		screen.render();
+		screen->clear();
+		
+		screen->draw(x, y, '0' + x);
+		screen->render();
 
 		//debugging
 		Borland::gotoxy(20, 20);
