03defa55b8ff621c051aa47a291554d531758f4c
janghona <jang1na2@gmail.com>
2021-09-17 22:06:20 +0900

    console에서 마우스와 키보드 제어 로직 추가
    * 콘솔창에서 Quick Edit Mode를 Disable 시켜줘야함
    * 필요한 부분만을 가져와서 사용할 줄 알아야함
    * MSDN 참조 https://docs.microsoft.com/en-us/windows/console/reading-input-buffer-events?redirectedfrom=MSDN

--- oop/oop/oop.cpp
+++ oop/oop/oop.cpp
@@ -28,7 +28,7 @@ public:
 
 	static Screen* getInstance() {
 		if (instance == nullptr) {
-			instance = new Screen(10,10);
+			instance = new Screen(80,20);
 		}
 		return instance;
 	}
@@ -60,9 +60,33 @@ public:
 
 Screen* Screen::instance = nullptr;
 
+HANDLE hStdin;
+DWORD fdwSaveOldMode;
+
+VOID ErrorExit(const char *);
+VOID KeyEventProc(KEY_EVENT_RECORD);
+VOID MouseEventProc(MOUSE_EVENT_RECORD);
+VOID ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD);
+
 int main()
 {
 	Screen* screen = Screen::getInstance();
+
+	DWORD cNumRead, fdwMode;
+	INPUT_RECORD irInBuf[128];
+
+	hStdin = GetStdHandle(STD_INPUT_HANDLE);
+	if (hStdin == INVALID_HANDLE_VALUE)
+		ErrorExit("GetStdHandle");
+
+	if (!GetConsoleMode(hStdin, &fdwSaveOldMode))
+		ErrorExit("GetConsoleMode");
+
+	// Enable the window and mouse input events. 
+
+	fdwMode = ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
+	if (!SetConsoleMode(hStdin, fdwMode))
+		ErrorExit("SetConsoleMode");
 
 	bool requestExit = false;
 	int x = 0, y = 0;
@@ -70,15 +94,117 @@ int main()
 	while (requestExit == false) {
 		screen->clear();
 		
+		if (!ReadConsoleInput(
+			hStdin,      // input buffer handle 
+			irInBuf,     // buffer to read into 
+			128,         // size of read buffer 
+			&cNumRead)) // number of records read 
+			ErrorExit("ReadConsoleInput");
+
+		// Dispatch the events to the appropriate handler. 
+		for (int i = 0; i < cNumRead; i++)
+		{
+			switch (irInBuf[i].EventType)
+			{
+			case KEY_EVENT: // keyboard input 
+				KeyEventProc(irInBuf[i].Event.KeyEvent);
+				break;
+
+			case MOUSE_EVENT: // mouse input 
+				MouseEventProc(irInBuf[i].Event.MouseEvent);
+				break;
+
+			case WINDOW_BUFFER_SIZE_EVENT: // scrn buf. resizing 
+				ResizeEventProc(irInBuf[i].Event.WindowBufferSizeEvent);
+				break;
+
+			case FOCUS_EVENT:  // disregard focus events 
+
+			case MENU_EVENT:   // disregard menu events 
+				break;
+
+			default:
+				ErrorExit("Unknown event type");
+				break;
+			}
+		}
+
 		screen->draw(x, y, '0' + x);
 		screen->render();
 
 		//debugging
-		Borland::gotoxy(20, 20);
-		printf("x = %d, y = %d", x, y);
+		Borland::gotoxy(0, 21);
+		printf("cNumRead = %d", cNumRead);
 
 		Sleep(100);
 	}
 	printf("\nGame Over\n");
 	return 0;
+}
+
+VOID ErrorExit(const char* lpszMessage){
+	Borland::gotoxy(0, 21);
+	printf("%80d\r", ' ');
+	printf("%s\n", lpszMessage);
+
+	// Restore input mode on exit.
+	SetConsoleMode(hStdin, fdwSaveOldMode);
+	ExitProcess(0);
+}
+
+VOID KeyEventProc(KEY_EVENT_RECORD ker){
+	Borland::gotoxy(0, 22);
+	printf("%80d\r", ' ');
+
+	printf("Key event: %c  %d        ", ker.uChar, ker.wRepeatCount);
+
+	if (ker.bKeyDown)
+		printf("key pressed\n");
+	else printf("key released\n");
+}
+
+VOID MouseEventProc(MOUSE_EVENT_RECORD mer){
+#ifndef MOUSE_HWHEELED
+#define MOUSE_HWHEELED 0x0008
+#endif
+	Borland::gotoxy(0, 22);
+	printf("%80d\r", ' ');
+
+	printf("Mouse event: %d %d      ", mer.dwMousePosition.X, mer.dwMousePosition.Y);
+
+	switch (mer.dwEventFlags){
+	case 0:
+		if (mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED){
+			printf("left button press \n");
+		}
+		else if (mer.dwButtonState == RIGHTMOST_BUTTON_PRESSED){
+			printf("right button press \n");
+		}
+		else{
+			printf("button press\n");
+		}
+		break;
+	case DOUBLE_CLICK:
+		printf("double click\n");
+		break;
+	case MOUSE_HWHEELED:
+		printf("horizontal mouse wheel\n");
+		break;
+	case MOUSE_MOVED:
+		printf("mouse moved\n");
+		break;
+	case MOUSE_WHEELED:
+		printf("vertical mouse wheel\n");
+		break;
+	default:
+		printf("unknown\n");
+		break;
+	}
+}
+
+VOID ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD wbsr){
+	Borland::gotoxy(0, 22);
+	printf("%80d\r", ' ');
+	printf("Resize event:             ");
+	printf("Console screen buffer is %d columns by %d rows.\n", wbsr.dwSize.X, wbsr.dwSize.Y);
 }
