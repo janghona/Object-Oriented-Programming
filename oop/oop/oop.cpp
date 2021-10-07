a77ce1785ff4cdb26171b6f56c782a337efa19fc
janghona <jang1na2@gmail.com>
2021-10-07 19:27:52 +0900

    독자적 MyString 클래스 구현 및 Panel UI Click기능 추가
    * string 헤더를 사용하지않고 독자적으로 만든 MyString 클래스를 만듬
    * 복사생성자 : 생성 시 복사 새로운 공간만들어서 사용(Deep copy) / 복사연산자 : 생성 후 복사 원래 있는 공간 사용함(shallow copy)
    * Panel을 minimize, restore할 수 있는 Icon 혹은 button을 추가함. Button은 Clickable 기능을 제공받아 click 이번트가 발생했을 때 이를 감지함

--- oop/oop/oop.cpp
+++ oop/oop/oop.cpp
@@ -4,14 +4,72 @@
 
 #include <iostream>
 #include <conio.h> // console io
-#include <cstring> // string.h
 #include <cstdlib> // stdlib.h
-#include <string> // c++ string class
 #include <Windows.h>
 #include<deque>
 #include<vector>
 #include"Utils.h"
 using namespace std;
+
+class MyString {
+private:
+	char* data;
+	int capacity;
+	
+	void resize(int capacity) {
+		if (this->capacity >= capacity) return;
+		char* newRegion = new char[capacity];
+		strcpy(newRegion, data);
+		delete[] data;
+		data = newRegion;
+		this->capacity = capacity;
+	}
+	MyString& append(const char* str) {
+		if (str == nullptr) return *this;
+		int len = strlen(data) + strlen(str) + 1;
+		while (this->capacity < len) resize(2 * this->capacity);
+		strcpy(&data[strlen(data)], str);
+		return *this;
+	}
+public:
+	MyString(const char* str, int capacity = 128)
+		: data(new char[capacity]), capacity(capacity)
+	{
+		int len = strlen(str) + 1;
+		while (this->capacity < len) resize(2 * this->capacity);
+		if (str == nullptr) {
+			data[0] = '\0';
+			return;
+		}
+		strcpy(data, str);
+	}
+	MyString(const MyString& other)
+		:MyString(other.data, max(128, strlen(other.data) + 1)) {}
+	~MyString() {
+		if (data) delete[] data;
+	}
+
+	MyString operator+(const MyString& str) { return *this + str.c_str(); }
+	MyString operator+(const char* str) { return MyString{ *this }.append(str); }
+	MyString& operator+=(const MyString& str) {
+		this->append(str.c_str());
+		return *this;
+	}
+
+	friend MyString operator+(const char* str, const MyString& other);
+	friend MyString to_mystring(int value);
+
+	const char* c_str() const { return data; }
+};
+
+MyString operator+(const char* str, const MyString& other) {
+	return MyString{ str } +other;
+}
+MyString to_mystring(int value) {
+	char buffer[11];
+	sprintf(buffer, "%d", value);
+	return MyString(buffer);
+}
 
 class Screen {
 private:
@@ -109,10 +167,6 @@ private:
 #ifndef MOUSE_HWHEELED
 #define MOUSE_HWHEELED 0x0008
 #endif
-		Borland::gotoxy(0, 22);
-		printf("%80d\r", ' ');
-		printf("Mouse event: %d %d      ", mer.dwMousePosition.X, mer.dwMousePosition.Y);
-
 		switch (mer.dwEventFlags) {
 		case 0:
 			if (mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED) {
@@ -148,8 +202,8 @@ private:
 		if (hStdin == INVALID_HANDLE_VALUE) ErrorExit("GetStdHandle");
 		FlushConsoleInputBuffer(hStdin);
 
-		string mode = "mode con:cols=" + to_string(Screen::getInstance()->getWidth() + 10);
-		mode += " lines=" + to_string(Screen::getInstance()->getHeight() + 5);
+		MyString mode = "mode con:cols=" + to_mystring(Screen::getInstance()->getWidth() + 10);
+		mode += " lines=" + to_mystring(Screen::getInstance()->getHeight() + 5);
 		std::system(mode.c_str());
 		std::system("chcp 437");
 
@@ -170,25 +224,31 @@ public:
 
 	bool GetKeyDown(WORD ch) {
 		if (events.empty() == true) return false;
-		INPUT_RECORD& in = events.front();
+		INPUT_RECORD in = events.front();
 		if (in.EventType != KEY_EVENT) return false;
 		if (in.Event.KeyEvent.bKeyDown = TRUE) {
 			return in.Event.KeyEvent.wVirtualKeyCode == ch;
 		}
 		return false;
 	}
+
+	Position mousePosition; // it will be used only when a mouse event occurs.
 	bool GetLeftMouseDown() {
 		if (events.empty() == true) return false;
-		const INPUT_RECORD& in = events.front();
+		const INPUT_RECORD in = events.front();
 		if (in.EventType != MOUSE_EVENT) return false;
+		mousePosition.x = in.Event.MouseEvent.dwMousePosition.X;
+		mousePosition.y = in.Event.MouseEvent.dwMousePosition.Y;
 		return in.Event.MouseEvent.dwEventFlags == 0
 			&& (in.Event.MouseEvent.dwButtonState & FROM_LEFT_1ST_BUTTON_PRESSED);
 	}
 
 	bool GetRightMouseDown() {
 		if (events.empty() == true) return false;
-		const INPUT_RECORD& in = events.front();
+		const INPUT_RECORD in = events.front();
 		if (in.EventType != MOUSE_EVENT) return false;
+		mousePosition.x = in.Event.MouseEvent.dwMousePosition.X;
+		mousePosition.y = in.Event.MouseEvent.dwMousePosition.Y;
 		return in.Event.MouseEvent.dwEventFlags == 0
 			&& (in.Event.MouseEvent.dwButtonState & RIGHTMOST_BUTTON_PRESSED);
 	}
@@ -218,11 +278,13 @@ InputManager* InputManager::instance = nullptr;
 
 class GameObject {
 private:
-	string shape;
+	MyString shape;
 	Position pos;
 
 	Position parentPos; // your parent's global position in space
 	bool dirty; // mark it TRUE if your local position is changed.
+
+	bool hidden;
 protected:
 	Screen& screen;
 	InputManager& inputManager;
@@ -231,14 +293,14 @@ protected:
 	GameObject* parent;
 	void setParentPos(const Position& parentPos) { this->parentPos = parentPos; }
 public:
-	GameObject(int x,int y,const string& shape,GameObject* parent = nullptr)
-		:shape(shape),pos(x,y),parent(parent),dirty(false),screen(*Screen::getInstance()),inputManager(*InputManager::getInstance())
+	GameObject(int x,int y,const MyString& shape,GameObject* parent = nullptr,bool hidden=false)
+		:shape(shape),pos(x,y),parent(parent),dirty(false),hidden(hidden),screen(*Screen::getInstance()),inputManager(*InputManager::getInstance())
 	{
 		setParentPos(parent ? parent->getWorldPos() : Position{ 0,0 });
 		if (parent) parent->add(this);
 	}
-	GameObject(const Position& pos, const string& shape, GameObject* parent = nullptr)
-		: GameObject(pos.x, pos.y, shape,parent) 
+	GameObject(const Position& pos, const MyString& shape, GameObject* parent = nullptr,bool hidden=false)
+		: GameObject(pos.x, pos.y, shape,parent,hidden) 
 	{
 	}
 	virtual ~GameObject()
@@ -258,7 +320,7 @@ public:
 	Position getPos() const { return pos; }
 	Position getWorldPos() const { return parentPos + pos; }
 	const char* getShape() const { return shape.c_str(); }
-	void setShape(const string& shape) { this->shape = shape; }
+	void setShape(const MyString& shape) { this->shape = shape; }
 
 	void setParent(GameObject* parent) {
 		this->parent = parent;
@@ -285,11 +347,99 @@ public:
 	virtual void update() {}
 
 	void internalDraw() {
+		if (hidden == true) return;
 		draw();
 		for (auto child : children) child->internalDraw();
 	}
 	virtual void draw() { screen.draw(getWorldPos(), shape.c_str()); }
+
+	// answer 2
+	bool isHidden() const { return hidden; }
+	void setHidden() { hidden = true; }
+	void setVisible() { hidden = false; }
+
+	// answer 3
+	virtual bool isOverlapped(const Position& position, const Position& size = Position{ 1,1 }) const {
+		Position pos = getWorldPos();
+		return pos.x == position.x && pos.y == position.y;
+	}
 };
+
+// we need to change the following as class SceneManager applying Singleton pattern
+vector<GameObject*> scene;
+
+// answer 4
+class Clickable : public GameObject {
+	Position size;
+
+public:
+	Clickable(const MyString& shape, const Position& pos, const Position& sz, GameObject* parent = nullptr, bool hidden = false)
+		: GameObject(pos, shape, parent, hidden), size(sz)
+	{}
+
+	void update() override {
+		if (inputManager.GetLeftMouseDown()) {
+			if (isOverlapped(inputManager.mousePosition)) {
+				onClick();
+			}
+		}
+	}
+
+	bool isOverlapped(const Position& position, const Position& sz = Position{ 1,1 }) const override {
+		Position pos = getWorldPos();
+		return (pos.x <= position.x && position.x < pos.x + size.x)
+			&& (pos.y <= position.y && position.y < pos.y + size.y);
+	}
+
+	virtual void onClick() {}
+};
+
+class RestoreButton : public Clickable {
+	GameObject* associated;
+public:
+	RestoreButton(const MyString& shape, const Position& pos, GameObject* panel, bool hidden = false)
+		: Clickable(shape, pos, Position{ 10, 1 }, nullptr, hidden), associated(panel)
+	{}
+
+	void onClick() override {
+		if (associated == nullptr) return;
+		associated->setVisible();
+		setHidden();
+	}
+	bool isMatching(GameObject* associated) { return associated == this->associated; }
+
+	void show() {
+		setVisible();
+		int numberOfRestoreButtons = 0;
+		for (auto obj : scene) {
+			auto restoreButton = dynamic_cast<RestoreButton *>(obj);
+			if (restoreButton == nullptr) continue;
+			if (restoreButton->isHidden() == false) numberOfRestoreButtons++;
+		}
+		this->setPos(screen.getWidth() - 9, screen.getHeight() - 2 - numberOfRestoreButtons);
+	}
+};
+
+class MinimizeIcon : public Clickable {
+public:
+	MinimizeIcon(const Position& pos, GameObject* parent, bool hidden = false)
+		: Clickable("\xB0", pos, Position{ 1,1 }, parent, hidden)
+	{}
+
+	void onClick() override {
+		if (parent == nullptr) return;
+		parent->setHidden();
+
+		for (auto obj : scene) {
+			auto restore = dynamic_cast<RestoreButton*>(obj);
+			if (restore == nullptr) continue;
+			if (restore->isMatching(this->parent)) {
+				restore->show();
+			}
+		}
+	}
+};
+
 
 class Block : public GameObject {
 private:
@@ -311,7 +461,7 @@ private:
 		size.x = h; size.y = w;
 	}
 public:
-	Block(const Position& pos, const string& shape,const Position& size,GameObject* parent = nullptr,bool rotatable = true)
+	Block(const Position& pos, const MyString& shape,const Position& size,GameObject* parent = nullptr,bool rotatable = true)
 		: GameObject(pos,shape,parent),size(size), internalShapeData(new char[size.x*size.y + 1]),rotatable(rotatable) 
 	{}
 	~Block() {
@@ -331,11 +481,22 @@ class Panel : public GameObject {
 private:
 	int width;
 	int height;
-	string title;
+	MyString title;
 public:
-	Panel(const string& title,const Position pos, int width, int height,GameObject* parent)
+	Panel(const MyString& title,const Position pos, int width, int height,GameObject* parent)
 		: GameObject(pos, "",parent), width(width), height(height),title(title)
 	{
+		auto minimizeIcon = new MinimizeIcon(Position{ width, -1 }, this, false);
+		children.push_back(minimizeIcon);
+
+		int numberOfRestoreButtons = 0;
+		for (auto obj : scene) {
+			auto restoreButton = dynamic_cast<RestoreButton *>(obj);
+			if (restoreButton == nullptr) continue;
+			numberOfRestoreButtons++;
+		}
+		scene.push_back(new RestoreButton(MyString{ "R " + to_mystring(numberOfRestoreButtons) },
+			Position{ 0, numberOfRestoreButtons }, this, true));
 	}
 
 	void update() override {
@@ -351,7 +512,7 @@ public:
 
 class Text : public GameObject {
 public:
-	Text(const Position& pos, const string& message, GameObject* parent)
+	Text(const Position& pos, const MyString& message, GameObject* parent)
 		: GameObject(pos, message.c_str(), parent)
 	{}
 	void update() override {
@@ -385,7 +546,7 @@ int main()
 	Screen& screen = *Screen::getInstance();
 	InputManager& inputmanager = *InputManager::getInstance();
 
-	vector<GameObject*> scene;
+	//vector<GameObject*> scene;
 
 	auto panel = new Panel{ "",Position{3,3}, 10, 20, nullptr };
 	new Block{ Position{4,0}, "\xdb  \xdb\xdb\xdb  \xdb", Position{ 3, 3},  panel };
